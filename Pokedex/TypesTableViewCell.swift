9c6808af2717b183794dc3bc86dc4265622f0a59
Daniel Carlander <carlander.daniel@gmail.com>
2018-10-30 17:01:34 +0100

    Type list

---
+++ Pokedex/TypesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TypesTableViewCell.swift
+//  Pokedex
+//
+//  Created by Daniel Carlander on 25/10/2018.
+//  Copyright © 2018 Daniel Carlander. All rights reserved.
+//
+
+import UIKit
+
+class TypesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var typeImage: UIImageView!
+    @IBOutlet weak var racesLabel: UILabel!
+    
+}
